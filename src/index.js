2e13508a7d0ce5b566056584e909c0727fe499db
KirylMikheyeu <40001510+KirylMikheyeu@users.noreply.github.com>
2022-06-23 14:44:20 +0300

    Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n > 0){
+        let arr = n.toString().split('');
+        return Number(arr.reverse().join(''));
+    } else  {
+           let arr = Math.abs(n).toString().split('');
+            return Number(arr.reverse().join(''));
+        }
+};
